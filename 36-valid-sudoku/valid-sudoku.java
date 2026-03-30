5d143aebbab042a7f5e8b777d76f38da9123a82a
Harsh Agarwal <harshagarwxl@gmail.com>
2026-03-30 13:59:40 +0530

    Time: 1 ms (99.92%) | Memory: 46.2 MB (94.55%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -16,120 +16,132 @@ class Solution {
                         return false;
                     }
                     // block
-                    if( i >= 0 && i <= 2 )
-                    {
-                        if(j >= 0 && j <= 2)
-                        {
-                            // block 1
-                            for( int k = 0 ; k < 3; k++ )
-                            {
-                                for( int l = 0; l<3;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else if ( j > 2 && j <= 5)
-                        {
-                            // block 2
-                            for( int k = 0 ; k < 3; k++ )
-                            {
-                                for( int l = 3; l<6;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            // block 3
-                            for( int k = 0 ; k < 3; k++ )
-                            {
-                                for( int l = 6; l<9;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                    else if ( i >= 3 && i <= 5 )
-                    {
-                        if(j >= 0 && j <= 2)
-                        {
-                            // block 1
-                            for( int k = 3 ; k < 6; k++ )
-                            {
-                                for( int l = 0; l<3;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else if ( j > 2 && j <= 5)
-                        {
-                            // block 2
-                            for( int k = 3 ; k < 6; k++ )
-                            {
-                                for( int l = 3; l<6;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            // block 3
-                            for( int k = 3 ; k < 6; k++ )
-                            {
-                                for( int l = 6; l<9;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if(j >= 0 && j <= 2)
-                        {
-                            // block 1
-                            for( int k = 6 ; k < 9; k++ )
-                            {
-                                for( int l = 0; l<3;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else if ( j > 2 && j <= 5)
-                        {
-                            // block 2
-                            for( int k = 6 ; k < 9; k++ )
-                            {
-                                for( int l = 3; l<6;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            // block 3
-                            for( int k = 6 ; k < 9; k++ )
-                            {
-                                for( int l = 6; l<9;l++)
-                                {
-                                    if(board[k][l] == ch && l != j)
-                                    return false;
-                                }
+                    // if( i >= 0 && i <= 2 )
+                    // {
+                    //     if(j >= 0 && j <= 2)
+                    //     {
+                    //         // block 1
+                    //         for( int k = 0 ; k < 3; k++ )
+                    //         {
+                    //             for( int l = 0; l<3;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else if ( j > 2 && j <= 5)
+                    //     {
+                    //         // block 2
+                    //         for( int k = 0 ; k < 3; k++ )
+                    //         {
+                    //             for( int l = 3; l<6;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else
+                    //     {
+                    //         // block 3
+                    //         for( int k = 0 ; k < 3; k++ )
+                    //         {
+                    //             for( int l = 6; l<9;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    // else if ( i >= 3 && i <= 5 )
+                    // {
+                    //     if(j >= 0 && j <= 2)
+                    //     {
+                    //         // block 1
+                    //         for( int k = 3 ; k < 6; k++ )
+                    //         {
+                    //             for( int l = 0; l<3;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else if ( j > 2 && j <= 5)
+                    //     {
+                    //         // block 2
+                    //         for( int k = 3 ; k < 6; k++ )
+                    //         {
+                    //             for( int l = 3; l<6;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else
+                    //     {
+                    //         // block 3
+                    //         for( int k = 3 ; k < 6; k++ )
+                    //         {
+                    //             for( int l = 6; l<9;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    // else
+                    // {
+                    //     if(j >= 0 && j <= 2)
+                    //     {
+                    //         // block 1
+                    //         for( int k = 6 ; k < 9; k++ )
+                    //         {
+                    //             for( int l = 0; l<3;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else if ( j > 2 && j <= 5)
+                    //     {
+                    //         // block 2
+                    //         for( int k = 6 ; k < 9; k++ )
+                    //         {
+                    //             for( int l = 3; l<6;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    //     else
+                    //     {
+                    //         // block 3
+                    //         for( int k = 6 ; k < 9; k++ )
+                    //         {
+                    //             for( int l = 6; l<9;l++)
+                    //             {
+                    //                 if(board[k][l] == ch && l != j)
+                    //                 return false;
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    // Block Check logic
+                    int startRow = (i / 3) * 3;
+                    int startCol = (j / 3) * 3;
+
+                    for (int r = startRow; r < startRow + 3; r++) {
+                        for (int c = startCol; c < startCol + 3; c++) {
+                            // Only check other cells in the block, not the current cell itself
+                            if (board[r][c] == ch && (r != i || c != j)) {
+                                return false;
                             }
                         }
                     }
