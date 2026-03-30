44aec6528ed73c98a08ae64cddd2ef51c785ecd9
Harsh Agarwal <harshagarwxl@gmail.com>
2026-03-30 13:54:16 +0530

    Time: 2 ms (66.39%) | Memory: 46.4 MB (77.75%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,142 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i = 0;i <9 ;i++)
+        {
+            for(int j = 0; j < 9 ; j++)
+            {
+                if(board[i][j] != '.')
+                {
+                    char ch = board[i][j];
+                    // rows & column check
+                    for( int k = 0; k < 9; k++)
+                    {
+                        if(board[k][j] == ch && k != i)
+                        return false;
+                        if(board [i][k] == ch && k != j)
+                        return false;
+                    }
+                    // block
+                    if( i >= 0 && i <= 2 )
+                    {
+                        if(j >= 0 && j <= 2)
+                        {
+                            // block 1
+                            for( int k = 0 ; k < 3; k++ )
+                            {
+                                for( int l = 0; l<3;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else if ( j > 2 && j <= 5)
+                        {
+                            // block 2
+                            for( int k = 0 ; k < 3; k++ )
+                            {
+                                for( int l = 3; l<6;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // block 3
+                            for( int k = 0 ; k < 3; k++ )
+                            {
+                                for( int l = 6; l<9;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    else if ( i >= 3 && i <= 5 )
+                    {
+                        if(j >= 0 && j <= 2)
+                        {
+                            // block 1
+                            for( int k = 3 ; k < 6; k++ )
+                            {
+                                for( int l = 0; l<3;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else if ( j > 2 && j <= 5)
+                        {
+                            // block 2
+                            for( int k = 3 ; k < 6; k++ )
+                            {
+                                for( int l = 3; l<6;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // block 3
+                            for( int k = 3 ; k < 6; k++ )
+                            {
+                                for( int l = 6; l<9;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(j >= 0 && j <= 2)
+                        {
+                            // block 1
+                            for( int k = 6 ; k < 9; k++ )
+                            {
+                                for( int l = 0; l<3;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else if ( j > 2 && j <= 5)
+                        {
+                            // block 2
+                            for( int k = 6 ; k < 9; k++ )
+                            {
+                                for( int l = 3; l<6;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // block 3
+                            for( int k = 6 ; k < 9; k++ )
+                            {
+                                for( int l = 6; l<9;l++)
+                                {
+                                    if(board[k][l] == ch && l != j)
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // method - 2  using method - duplicate  3 blocks and 9 columns & 9 rows
+        return true;
+    }
+}
