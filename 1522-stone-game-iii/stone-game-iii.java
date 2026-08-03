f7f0884d2f84e0eae3654dad74557b6b1664ae9c
Harsh Agarwal <harshagarwxl@gmail.com>
2026-08-04 00:54:04 +0530

    Time: 10 ms (83.24%) | Memory: 85.6 MB (65.91%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,27 +1,20 @@
 class Solution {
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        int[] dp = new int[n + 1]; 
-        // dp[n] = 0 (Base case: no stones left, net difference is 0)
-
+        int[] dp = new int[n + 1];
         for (int i = n - 1; i >= 0; i--) {
-            int takeSum = 0;
-            dp[i] = Integer.MIN_VALUE;
-
-            // Try taking 1, 2, or 3 stones
+           dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
             for (int k = 0; k < 3 && i + k < n; k++) {
-                takeSum += stoneValue[i + k];
-                dp[i] = Math.max(dp[i], takeSum - dp[i + k + 1]);
-            }
-        }
-
-        // Alice starts at index 0
+                sum += stoneValue[i + k];
+                dp[i] = Math.max(dp[i], sum - dp[i + k + 1]);
+            }}
         if (dp[0] > 0) {
-            return "Alice";
-        } else if (dp[0] < 0) {
-            return "Bob";
-        } else {
-            return "Tie";
+         return "Alice";
         }
+        if (dp[0] < 0) {
+         return "Bob";
+        }
+     return "Tie";
     }
 }
