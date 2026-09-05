047d779ff6309eede7f6edac579cb55d21f4c03f
Harsh Agarwal <harshagarwxl@gmail.com>
2026-09-05 15:26:17 +0530

    Time: 1 ms (99.79%) | Memory: 46.6 MB (15.79%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -1,24 +1,15 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        // int stablecount = 0;
-        int n = nums.length;
-        if(n == 0) return -1;
-        int[] suffixMin = new int[n];
-        suffixMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
-        }
-        int prefixMax = Integer.MIN_VALUE;
+        int n = nums.length, max = 0;
+        int[] min = new int[n];
+        min[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            min[i] = Math.min(nums[i], min[i + 1]);
         for (int i = 0; i < n; i++) {
-            prefixMax = Math.max(prefixMax, nums[i]);
-            
-            long instabilityScore = (long) prefixMax - suffixMin[i];
-            
-            if (instabilityScore <= k) {
-                return i; 
-            }
+            max = Math.max(max, nums[i]);
+            if (max - min[i] <= k)
+                return i;
         }
-
         return -1;
     }
 }
