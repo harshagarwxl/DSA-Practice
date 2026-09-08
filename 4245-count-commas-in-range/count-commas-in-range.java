5c34ada91cfaf118baff7e6704b06b7bde03db39
Harsh Agarwal <harshagarwxl@gmail.com>
2026-09-08 13:12:57 +0530

    Time: 1 ms (83.65%) | Memory: 42.4 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,8 +1,10 @@
 class Solution {
     public int countCommas(int n) {
-        if( n > 999)
-        return n - 999;
-        // else 
-        return 0;
+        // if( n > 999)
+        // return n - 999;
+        // // else 
+        // return 0;
+
+        return n>999 ? n - 999: 0;
     }
 }
