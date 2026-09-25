f59c7c9c825fa620d7035b509bf151ca81e76900
Harsh Agarwal <harshagarwxl@gmail.com>
2026-09-25 15:38:30 +0530

    Time: 4 ms (19.91%) | Memory: 42.9 MB (51.69%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean [] arr = new boolean [26];
+        Arrays.fill(arr, false);
+        sentence.toLowerCase();
+        for(char ch : sentence.toCharArray()) {
+            if(!Character.isWhitespace(ch)) {
+                int d = ch - 'a';
+                arr[d] = true;
+            }
+
+        }
+        for(boolean c : arr) {
+            if( c == false)
+            return false;
+        }
+
+        return true;
+    }
+}
