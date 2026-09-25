6ce153ec3448155806a63ea70d1a3cfc8aa7db4b
Harsh Agarwal <harshagarwxl@gmail.com>
2026-09-25 15:44:59 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.69%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean [] arr = new boolean [26];
-        Arrays.fill(arr, false);
-        sentence.toLowerCase();
-        for(char ch : sentence.toCharArray()) {
-            if(!Character.isWhitespace(ch)) {
-                int d = ch - 'a';
-                arr[d] = true;
-            }
+    //     boolean [] arr = new boolean [26];
+    //     Arrays.fill(arr, false);
+    //     sentence.toLowerCase();
+    //     for(char ch : sentence.toCharArray()) {
+    //         if(!Character.isWhitespace(ch)) {
+    //             int d = ch - 'a';
+    //             arr[d] = true;
+    //         }
 
-        }
-        for(boolean c : arr) {
-            if( c == false)
-            return false;
-        }
+    //     }
+    //     for(boolean c : arr) {
+    //         if( c == false)
+    //         return false;
+    //     }
 
-        return true;
+    //     return true;
+    // }
+    if( sentence.length() < 26)
+    return false;
+
+    for( char ch = 'a' ; ch <= 'z'; ch++){
+        if(sentence.indexOf(ch) <= -1)
+        return false;
+    }
+    return true;
+
+
     }
 }
